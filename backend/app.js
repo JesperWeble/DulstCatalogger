9a1cbfa7c27cccbb7aaec2288acc7f9742f3a1c8
Jesper Webel <jesperweble@outlook.com>
2025-06-20 11:16:46 +0200

    now fetches the yml code of each file

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 import fs from 'fs/promises';
 import {token, projectId} from './config.js'
+import YAML from 'yaml';
 
 const ref = 'master';
 const repositoryPath = `https://dev.dulst.com/api/v4/projects/${projectId}/repository`
 
 
 
-async function fetchPage(cardType, page)
+async function fetchTree(cardType)
 {
     const treePath = encodeURIComponent(`src/cards/${cardType}`);
     const treeUrl = `${repositoryPath}/tree?path=${treePath}&ref=${ref}`
@@ -21,11 +22,14 @@ async function fetchPage(cardType, page)
             }
         });
         let data = await response.json();
-        allCards = allCards.concat(data); // Concatenate the new data to an array for saving it to json file.
-        data.forEach(card => 
+        for (const file of data) 
         {
-            console.log(card.name);
-        });
+            const card = await fetchFile(cardType, file.name)
+            console.log(card)
+            allCards.push(card);
+
+
+        };
         if (data.length < 100) 
         {
             break;
@@ -36,4 +40,22 @@ async function fetchPage(cardType, page)
     console.log(`Saved ${cardType}.json`);
 }
 
-fetchPage('unit', 1);
+async function fetchFile(cardType, fileName)
+{
+    const filePath = encodeURIComponent(`src/cards/${cardType}/${fileName}`);
+    const fileUrl = `${repositoryPath}/files/${filePath}/raw?ref=${ref}`
+    const response = await fetch(fileUrl, 
+    {
+        headers: 
+        {
+            'PRIVATE-TOKEN': token
+        }
+    });
+    const data = await response.text();
+    const parsedData = YAML.parse(data);
+    return parsedData
+}
+
+// fetchFile('unit', 'Adored City Girl 65556937.yml');
+fetchTree('unit');
+

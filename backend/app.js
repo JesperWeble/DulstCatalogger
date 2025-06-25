a57cecef8ed80f874a013d64d5c66a765a856e31
Jesper Webel <jesperweble@outlook.com>
2025-06-25 10:55:14 +0200

    separated into different files for readability.

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
-import fs from 'fs/promises';
-import {token, projectId} from './config.js'
-import YAML from 'yaml';
+import * as fetching from './fetching.js';
+import * as exporting from './exporting_toExcel.js';
 
-const ref = 'master';
-const repositoryPath = `https://dev.dulst.com/api/v4/projects/${projectId}/repository`
-const cardTypesToExtract = ['unit', 'event', 'twist', 'dream', 'equip', 'member'];
+const cardTypesToExtract = ['hero', 'unit', 'event', 'twist', 'dream', 'equip', 'member'];
+
+
 const propsToExtract = ['title', 'cost', 'atk', 'health', 'effectsDescription', 'expansion', 'reqSource'];
-const pageSize = 100; // Number of items per page
+
 
 
 // async function fetchTree(cardType)
@@ -41,89 +40,10 @@ const pageSize = 100; // Number of items per page
 //     console.log(`Saved ${cardType}.json`);
 // }
 
-async function fetchTree(cardType) // Fetches Multiple Pages at the same time test.
-{
-    const treePath = encodeURIComponent(`src/cards/${cardType}`);
-    const treeUrl = `${repositoryPath}/tree?path=${treePath}&ref=${ref}`
-    let allCards = []; // Array to hold all cards fetched from the API.
-    let page = 1;
-    const batchSize = 5; // Number of pages to fetch concurrently
-    while (true) // creates an array like [1,2,3,4,5... up to batchSize]
-    {
-        const pageNumbers = [];
-        for (let i = 0; i < batchSize; i++) 
-        {
-            pageNumbers.push(page + i);
-        }
 
-        // Fetch pages concurrently
-        const pagesData = await Promise.all
-        (
-            pageNumbers.map(currentPage =>
-                fetch(`${treeUrl}&per_page=${pageSize}&page=${currentPage}`,
-                {
-                    headers: 
-                    {
-                        'PRIVATE-TOKEN': token
-                    }
-                })
-                .then(response => response.json() ) // Convert each response to JSON
-            )
-        );
 
-        const flattenedBatch = pagesData.flat();
-        const allFiles = await Promise.all
-        (
-            flattenedBatch.map(file => fetchFile(cardType, file.name))
-        );
-        const filteredCards = allFiles.filter(card => card !== null);
-        allCards.push(...filteredCards);
-        if (flattenedBatch.length < pageSize) 
-        {
-            break; // Exit the loop if the last batch has fewer items than pageSize
-        }
-        page += batchSize; // Move to the next batch of pages
-        
-    };
-    console.log(`Fetched ${allCards.length} cards of type ${cardType}`);
-    await fs.writeFile(`${cardType}.json`, JSON.stringify(allCards, null, 2));
-    console.log(`Saved ${cardType}.json`);
-}
 
-async function fetchFile(cardType, fileName)
-{
-    const filePath = encodeURIComponent(`src/cards/${cardType}/${fileName}`);
-    const fileUrl = `${repositoryPath}/files/${filePath}/raw?ref=${ref}`
-    const response = await fetch(fileUrl, 
-    {
-        headers: 
-        {
-            'PRIVATE-TOKEN': token
-        }
-    });
-    const data = await response.text();
-    if (data.includes('version: alpha')) 
-    {
-        console.log(`Skipping ${fileName} due to alpha version`);
-        return null; // Skip files with 'version: alpha'
-    }
-    else
-    {
-        const filteredData = data
-            .split('\n')
-            .filter(line => 
-                propsToExtract.some(prop => line.startsWith(`${prop}:`))
-            );
-        const parsedData = YAML.parse(filteredData.join('\n'));
-        return parsedData
-    }
-}
-async function fetchAllCards(cardTypes)
-{
-    
-    const promises = cardTypes.map(cardType => fetchTree(cardType));
-    await Promise.all(promises);
-}
+
 console.time('Total Time Elapsed');
-await fetchAllCards(cardTypesToExtract);
+await fetching.fetchAllCards(cardTypesToExtract, propsToExtract);
 console.timeEnd('Total Time Elapsed');
---
+++ backend/fetching.js
@@ -0,0 +1,92 @@
+import fs from 'fs/promises';
+import {token, projectId} from './config.js';
+import YAML from 'yaml';
+
+const ref = 'master';
+const repositoryPath = `https://dev.dulst.com/api/v4/projects/${projectId}/repository`
+const pageSize = 100; // Number of items per page
+
+export async function fetchTree(cardType, propsToExtract)
+{
+    const treePath = encodeURIComponent(`src/cards/${cardType}`);
+    const treeUrl = `${repositoryPath}/tree?path=${treePath}&ref=${ref}`
+    let allCards = []; // Array to hold all cards fetched from the API.
+    let page = 1;
+    const batchSize = 5; // Number of pages to fetch concurrently
+    while (true) // creates an array like [1,2,3,4,5... up to batchSize]
+    {
+        const pageNumbers = [];
+        for (let i = 0; i < batchSize; i++) 
+        {
+            pageNumbers.push(page + i);
+        }
+
+        // Fetch pages concurrently
+        const pagesData = await Promise.all
+        (
+            pageNumbers.map(currentPage =>
+                fetch(`${treeUrl}&per_page=${pageSize}&page=${currentPage}`,
+                {
+                    headers: 
+                    {
+                        'PRIVATE-TOKEN': token
+                    }
+                })
+                .then(response => response.json() ) // Convert each response to JSON
+            )
+        );
+
+        const flattenedBatch = pagesData.flat();
+        const allFiles = await Promise.all
+        (
+            flattenedBatch.map(file => fetchFile(cardType, propsToExtract, file.name))
+        );
+        const filteredCards = allFiles.filter(card => card !== null);
+        allCards.push(...filteredCards);
+        if (flattenedBatch.length < pageSize) 
+        {
+            break; // Exit the loop if the last batch has fewer items than pageSize
+        }
+        page += batchSize; // Move to the next batch of pages
+        
+    };
+    console.log(`Fetched ${allCards.length} cards of type ${cardType}`);
+    await fs.writeFile(`${cardType}.json`, JSON.stringify(allCards, null, 2));
+    console.log(`Saved ${cardType}.json`);
+}
+
+export async function fetchFile(cardType, propsToExtract, fileName)
+{
+    const filePath = encodeURIComponent(`src/cards/${cardType}/${fileName}`);
+    const fileUrl = `${repositoryPath}/files/${filePath}/raw?ref=${ref}`
+    const response = await fetch(fileUrl, 
+    {
+        headers: 
+        {
+            'PRIVATE-TOKEN': token
+        }
+    });
+    const data = await response.text();
+    if (data.includes('version: alpha')) 
+    {
+        console.log(`Skipping ${fileName} due to alpha version`);
+        return null; // Skip files with 'version: alpha'
+    }
+    else
+    {
+        const filteredData = data
+            .split('\n')
+            .filter(line => 
+                propsToExtract.some(prop => line.startsWith(`${prop}:`))
+            );
+        const parsedData = YAML.parse(filteredData.join('\n'));
+        return parsedData
+    }
+}
+
+export async function fetchAllCards(cardTypes, propsToExtract)
+{
+    
+    const promises = cardTypes.map(cardType => fetchTree(cardType, propsToExtract));
+    await Promise.all(promises);
+}
